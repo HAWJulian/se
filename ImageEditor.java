9d9813634745e96edd404fe9802e864d3e453d63
Dirk Löwenstrom <d.loewenstrom@web.de>
2015-05-07 11:14:17 +0200

    added menubar creation

--- ImageEditor.java
+++ ImageEditor.java
@@ -7,12 +7,29 @@ import javax.imageio.*;
 
 public class ImageEditor extends JFrame{ 
 	public ImageEditor(){ 
+	createMenuBar();
 	setDefaultCloseOperation(EXIT_ON_CLOSE); 
 	setSize(400, 300); 
-	setVisible(true); } 
+	setVisible(true); 
+	} 
 	
 	public static void main(String[] args){
 		ImageEditor frame = new ImageEditor();
+	}
+	
+	private void createMenuBar(){
+		JMenuBar menuBar = new JMenuBar(); 
+		setJMenuBar(menuBar);
+		JMenu menuFile = new JMenu("File"); 
+		menuBar.add(menuFile);
+		JMenuItem menuItemOpen = new JMenuItem("Open"); 
+		menuFile.add(menuItemOpen);
+		menuItemOpen.addActionListener(e -> {
+			onOpen();
+		});
+	}
+	private void onOpen(){
+ 		JOptionPane.showMessageDialog(this, "Open Selected");
 	}
 }
 	
